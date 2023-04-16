7ffdf663c6acbfcfb63bbb46c7cb50c09b3d1e82
Anime-Manga <>
2023-04-16 11:23:51 +0000

    Update from https://github.com/Anime-Manga/references/commit/2832de1c972f069360d4aa9e3e0b11fe0ee4695d

---
+++ src/references/Cesxhin.AnimeManga.Application/Services/AccountService.cs
@@ -0,0 +1,104 @@
+﻿using Cesxhin.AnimeManga.Application.Generic;
+using Cesxhin.AnimeManga.Application.Interfaces.Repositories;
+using Cesxhin.AnimeManga.Application.Interfaces.Services;
+using Cesxhin.AnimeManga.Domain.DTO;
+using Cesxhin.AnimeManga.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cesxhin.AnimeManga.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        public readonly IAccountRepository _accountRepository;
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        private static bool validPassword(string password, string hasedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hasedPassword);
+        }
+
+        private static string cryptPasword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public async Task<AuthDTO> CreateAccount(string username, string password)
+        {
+            if(await _accountRepository.findAccountByUsername(username) == null)
+            {
+                var auth = new Auth
+                {
+                    Username = username,
+                    Password = password
+                };
+
+               auth.Password = cryptPasword(auth.Password);
+               var authResult = await _accountRepository.CreateAccount(auth);
+
+               return AuthDTO.AuthToAuthDTO(authResult);
+            }
+
+            return null;
+        }
+
+        public async Task<AuthDTO> Login(string username, string password)
+        {
+            var findUser = await _accountRepository.findAccountByUsername(username);
+
+            if (findUser != null && validPassword(password, findUser.Password))
+                return AuthDTO.AuthToAuthDTO(findUser);
+
+            return null;
+        }
+
+        public async Task<WatchListDTO> InsertWatchList(WatchListDTO whiteListDTO)
+        {
+            if(!await _accountRepository.whiteListCheckByName(whiteListDTO.Name))
+            {
+                var result = await _accountRepository.InsertWhiteList(WatchList.WatchListDTOToWatchList(whiteListDTO));
+                if (result != null)
+                    return WatchListDTO.WatchListToWatchListDTO(result);
+            }
+            return null;
+        }
+
+        public async Task<WatchListDTO> DeleteWatchList(WatchListDTO whiteListDTO)
+        {
+            var result = await _accountRepository.DeleteWhiteList(WatchList.WatchListDTOToWatchList(whiteListDTO));
+
+            if (result != null)
+                return WatchListDTO.WatchListToWatchListDTO(result);
+            return null;
+        }
+
+        public async Task<bool> WatchListCheckByName(string name)
+        {
+            return await _accountRepository.whiteListCheckByName(name);
+        }
+
+        public async Task<IEnumerable<WatchListDTO>> GetListWatchListByUsername(string username)
+        {
+            if(username != null)
+            {
+                var result = await _accountRepository.GetListWatchListByUsername(username);
+
+                if(result != null)
+                {
+                    var resultArray = new List<WatchListDTO>();
+
+                    foreach(var item in result)
+                    {
+                        resultArray.Add(WatchListDTO.WatchListToWatchListDTO(item));
+                    }
+
+                    return resultArray;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ src/references/Cesxhin.AnimeManga.Persistence/Repositories/AccountRepository.cs
@@ -0,0 +1,136 @@
+﻿using Cesxhin.AnimeManga.Application.Interfaces.Repositories;
+using Cesxhin.AnimeManga.Application.NlogManager;
+using Cesxhin.AnimeManga.Domain.Models;
+using NLog;
+using Npgsql;
+using RepoDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cesxhin.AnimeManga.Persistence.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        //log
+        private readonly NLogConsole _logger = new(LogManager.GetCurrentClassLogger());
+
+        //env
+        readonly string _connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION");
+
+        public async Task<Auth> CreateAccount(Auth auth)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    await connection.InsertAsync(auth);
+                    return auth;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error($"Failed CreateAccount, details error: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public async Task<WatchList> DeleteWhiteList(WatchList whiteList)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    var rs = await connection.DeleteAsync(whiteList);
+
+                    if (rs > 0)
+                        return whiteList;
+                    return null;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error($"Failed DeleteWhiteList, details error: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public async Task<Auth> findAccountByUsername(string username)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    var rs = await connection.QueryAsync<Auth>(e => e.Username == username);
+
+                    if (rs != null && rs.Count() > 0)
+                        return rs.First();
+                    return null;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error($"Failed findAccountByUsername, details error: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public async Task<IEnumerable<WatchList>> GetListWatchListByUsername(string username)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    var rs = await connection.QueryAsync<WatchList>(e => e.Username == username);
+
+                    if (rs != null && rs.Count() > 0)
+                        return rs;
+                    return null;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error($"Failed GetListWatchListByUsername, details error: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public async Task<WatchList> InsertWhiteList(WatchList whiteList)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    await connection.InsertAsync(whiteList);
+                    return whiteList;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error($"Failed InsertWhiteList, details error: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        public async Task<bool> whiteListCheckByName(string name)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    var rs = await connection.QueryAsync<WatchList>(e => e.Name == name);
+
+                    if(rs != null && rs.Count() > 0)
+                        return true;
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error($"Failed InsertWhiteList, details error: {ex.Message}");
+                    return false;
+                }
+            }
+        }
+    }
+}
